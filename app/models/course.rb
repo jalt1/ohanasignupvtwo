e51b1c564f74c101c1b21a07579badd0163b3248
jalt1 <jocelyn@ohanaparents.com>
2013-12-04 16:52:34 -0600

    Added Validations to Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,10 @@ class Course < ActiveRecord::Base
   belongs_to :category
   has_many :users, :through => :slots
 
+  validates :start_date, :presence => true
+
+  validates :end_date, :presence => true
+
   def full_course_title
     return "#{category.name}: #{start_date.strftime("%b %e, %Y")} - #{end_date.strftime("%b %e, %Y")}"
   end
