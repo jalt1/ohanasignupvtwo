341006ab3c755db26df40bac4334e6d926ca1cfa
jalt1 <jocelyn@ohanaparents.com>
2013-12-04 16:51:07 -0600

    Added Validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
     return "#{first_name} #{last_name}"
   end
 
+  validates :first_name, :presence => true
+
+  validates :last_name, :presence => true
+
+  validates :email, :presence => true
+
+  validates :phone, :presence => true
+
 end
