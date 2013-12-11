1ac2e51624f4fcf73c25a22fad1914e786c36b77
jalt1 <jocelyn@ohanaparents.com>
2013-12-10 18:35:48 -0600

    Fixed a few view glitches and added better redirect for User New action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     @user.password_digest = params[:password_digest]
 
     if @user.save
-      redirect_to users_url, notice: "User created successfully."
+      redirect_to categories_url, notice: "Thank you! Your registration has been completed successfully!"
     else
       render 'new'
     end
@@ -66,13 +66,11 @@ class UsersController < ApplicationController
     @user.baby_gender = params[:baby_gender]
     @user.password_digest = params[:password_digest]
 
-
-
-
-
-    if params[:course_id] != @user.slot.course_id
-      @user.slot.available = true
-      @user.slot.user_id = nil
+    @user.slots.each do |slot|
+      slot.available = true
+      slot.user_id = nil
+      slot.save
+      end
 
       c = Course.find(params[:course_id])
       c.slots.each do |slot|
@@ -83,7 +81,6 @@ class UsersController < ApplicationController
           break
         end
       end
-    end
 
     if @user.save
       redirect_to users_url, notice: "User updated successfully."
@@ -91,25 +88,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
 
-
-    # current_slot = @user.slots
-    # current_slot.each do |slot|
-    #   slot.available = true
-    #   slot.save
-    #  end
-
-    # course = Course.find_by(:id => params[:course_id])
-    # course.slots.each do |slot|
-    #   if slot.available?
-    #   slot.user_id = @user.id
-    #   slot.available = false
-    #   slot.save
-    #   break
-    #   end
-    #   end
-
-    #@user.course =
-  end
+   end
 
   def destroy
     @user = User.find_by(id: params[:id])
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ActiveRecord::Base
   has_many :courses
+
+  validates :name, :presence => true
+
+  validates :description, :presence => true
+
+  validates :image, :presence => true
 end
